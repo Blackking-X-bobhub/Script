d950ce874ea49d26bd83aeb3045e2245caa18063
zano <zanocoderversion@gmail.com>
2025-08-11 06:26:16 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local placeid = game.PlaceId
 
 if placeid == 6839171747 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackking-X-bobhub/Script/refs/heads/main/Games/Doors.lua'))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackking-X-bobhub/Script/refs/heads/main/Games/Protected_5420372766929215.lua.txt"))()
 end
