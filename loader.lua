ea66ca18f18f62453a66aa67b5f44b5049deb811
zano <zanocoderversion@gmail.com>
2025-08-10 21:48:00 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local placeid = game.PlaceId
+
+if placeid == 6839171747 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Blackking-X-bobhub/Script/main/Place/Doors.lua'))()
+end
